72b0870c9ea8ac461fc648519cc84c713807e80c
lsantanar <contatolsr@hotmail.com>
2022-11-22 22:14:29 -0300

    add more informations from index.js

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const prompt = require( "prompt" );
+
+const schema = {
+  properties: {
+    name: {
+      description: "Informe seu nome",
+      message: "Utilize apenas letras",
+      pattern: /^[a-zA-Z\s\-]+$/,
+      required: true,
+    },
+    lastname: {
+      description: "Informe seu sobrenome",
+      message: "Utilize apenas letras",
+      pattern: /^[a-zA-Z\s\-]+$/,
+      required: true,
+    },
+    email: {
+      description: "Informe seu email",
+      message: "Preencha um email válido",
+      required: true,
+    },
+    cpf: {
+      description: "Informe seu CPF",
+      message: "Preencha um CPF válido",
+      pattern: /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+      required: true,
+    },
+  },
+};
 prompt.start();
-prompt.get([{ name: "primeiroNome", required: true },
-            { name: "primeiroNome", required: true }
-], function (_, result) {
-    console.log( "Nome: " + result.name);
+
+prompt.get(schema, function (err, result) {
+  console.log("Resumo:");
+  console.log("  Nome: " + result.name);
+  console.log("  Sobrenome: " + result.lastname);
+  console.log("  E-mail: " + result.email);
+  console.log("  CPF: " + result.cpf);
 });
